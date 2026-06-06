14377e7a8e681ba1077b40cf956f8e3d7133da6c
PAEvpatov <paevpatov@mai.education>
2026-06-06 15:38:40 +0300

    Восстановлена работоспособность сервера

--- 8080/src/main/java/com/example/sputnik_onegina/KafkaService.java
+++ 8080/src/main/java/com/example/sputnik_onegina/KafkaService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Slf4j
